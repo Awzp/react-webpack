c2e76b06e523f75b1e448ce057d05c7168c7d888
wangzhongpu <wangzhongpu@moxiu.com>
2018-04-28 19:03:00 +0800

    引入css

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { LocaleProvider, DatePicker, message } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN';
 import moment from 'moment';
 import 'moment/locale/zh-cn';
-
+import './index.css';
 moment.locale('zh-cn');
 
 class App extends React.Component {
