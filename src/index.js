50fdd9f45ef4b671ec98463b10b5218a55bb99f7
wangzhongpu <wangzhongpu@moxiu.com>
2018-08-24 19:14:21 +0800

    test 测试jenkins

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ class SiderDemo extends React.Component {
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
             <Menu.Item key="1">
               <Icon type="pie-chart" />
-              <span>Option 1</span>
+              <span>Option 123</span>
             </Menu.Item>
             <Menu.Item key="2">
               <Icon type="desktop" />
